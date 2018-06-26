8201ed8b7e63a44a7944f95129c3c3a97730060d
Ayrton Araujo <root@ayr-ton.net>
2018-06-26 09:54:45 -0300

    Add DNS manipulation with terraform

---
+++ dns/main.tf
@@ -0,0 +1,36 @@
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+
+provider "cloudflare" {
+  email = "${var.cloudflare_email}"
+  token = "${var.cloudflare_token}"
+}
+
+variable "domain" { default = "bolado.network" }
+variable "local_ip" {}
+variable "lula_ip" {}
+
+resource "cloudflare_record" "csd" {
+  domain = "${var.domain}"
+  name = "csd"
+  value = "${var.local_ip}"
+  type = "A"
+  ttl = "120"
+}
+
+resource "cloudflare_record" "git" {
+  domain = "${var.domain}"
+  name = "git"
+  value = "${var.local_ip}"
+  type = "A"
+  ttl = "120"
+}
+
+resource "cloudflare_record" "lula" {
+  domain = "${var.domain}"
+  name = "lula"
+  value = "${var.lula_ip}"
+  type = "A"
+  ttl = "120"
+}
+
